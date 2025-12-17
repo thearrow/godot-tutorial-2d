017a8637794030c2c620e36cd953a744cf17ac7b
Jake Klingler <thearrow3456@gmail.com>
2025-12-16 20:50:40 -0500

    HUD and main gameplay loop

--- main.gd
+++ main.gd
@@ -5,7 +5,7 @@ var score
 
 # called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_game()
+	pass
 
 # called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -14,11 +14,16 @@ func _process(delta: float) -> void:
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game() -> void:
+	# delete all old mobs from previous game
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_mob_timer_timeout() -> void:
 	# create a new instance of the Mob scene
@@ -47,6 +52,7 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
